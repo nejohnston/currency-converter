5bad643986db3f26cebcc5f9ea1df5f2f11e21f4
Nicholas Johnston <nicholas.johnston94@gmail.com>
2020-01-03 22:09:54 -0800

    added before_space and after_space functions and tests

---
+++ currency.py
@@ -0,0 +1,60 @@
+"""
+Module for currency exchange
+
+This module provides several string parsing functions to implement a simple
+currency exchange routine using an online currency service. The primary function
+in this module is exchange().
+
+Author: Nicholas Johnston
+Date:   January 3rd 2020
+"""
+
+import introcs
+
+APIKEY = "8mNy0uxE2kaogoJoprIscPANUb8YWurAFq0Y84q9oLIe"
+
+
+def before_space(s):
+    """
+    Returns the substring of s up to, but not including, the first space.
+
+    Example: before_space('Hello World') returns 'Hello'
+
+    Parameter s: the string to slice
+    Precondition: s is a string with at least one space in it
+    """
+    assert introcs.find_str(s, ' ') >= 0
+    space_index = introcs.index_str(s, ' ')
+    split_string = s[:space_index]
+    return split_string
+
+
+def after_space(s):
+    """
+    Returns the substring of s after the first space
+
+    Example: before_space('Hello World') returns 'World'
+
+    Parameter s: the string to slice
+    Precondition: s is a string with at least one space in it
+    """
+    assert introcs.find_str(s, ' ') >= 0
+    space_index = introcs.index_str(s, ' ')
+    split_string = s[space_index+1:]
+    return split_string
+
+
+def first_inside_quotes(s):
+    """
+    Returns the first substring of s between two (double) quote characters
+
+    Note that the double quotes must be part of the string.  So "Hello World" is a 
+    precondition violation, since there are no double quotes inside the string.
+
+    Example: first_inside_quotes('A "B C" D') returns 'B C'
+    Example: first_inside_quotes('A "B C" D "E F" G') returns 'B C', because it only 
+    picks the first such substring.
+
+    Parameter s: a string to search
+    Precondition: s is a string with at least two (double) quote characters inside
+    """
---
+++ testcurrency.py
@@ -0,0 +1,117 @@
+"""
+Unit tests for module currency
+
+When run as a script, this module invokes several procedures that test
+the various functions in the module currency.
+
+Author: Nicholas Johnston
+Date:   January 3rd 2020
+"""
+
+import introcs
+import currency
+
+
+def test_before_space():
+    """
+    Test procedure for before_space
+    """
+    print('Testing before_space')
+    result = currency.before_space('    ')
+    introcs.assert_equals('', result)
+    result = currency.before_space('Nicholas ')
+    introcs.assert_equals('Nicholas', result)
+    result = currency.before_space(' Nicholas')
+    introcs.assert_equals('', result)
+    result = currency.before_space('N i c h o l a s')
+    introcs.assert_equals('N', result)
+
+
+def test_after_space():
+    """
+    Test procedure for after_space
+    """
+    print('Testing after_space')
+    result = currency.after_space('  ')
+    introcs.assert_equals(' ', result)
+    result = currency.after_space('Nicholas ')
+    introcs.assert_equals('', result)
+    result = currency.after_space('Nich olas')
+    introcs.assert_equals('olas', result)
+    result = currency.after_space(' Nicholas')
+    introcs.assert_equals('Nicholas', result)
+    result = currency.after_space('N i c h o l a s')
+    introcs.assert_equals('i c h o l a s', result)
+
+
+def test_first_inside_quotes():
+    """
+    Test procedure for first_inside_quotes
+    """
+    print('Testing first_inside_quotes')
+    result = currency.first_inside_quotes('A "B C" D')
+    introcs.assert_equals('B C', result)
+    result = currency.first_inside_quotes('A "B C" D "E F" G')
+    introcs.assert_equals('B C', result)
+    result = currency.first_inside_quotes('""')
+    introcs.assert_equals('', result)
+    result = currency.first_inside_quotes('a""b""c')
+    introcs.assert_equals('', result)
+    result = currency.first_inside_quotes('''"abc"''')
+    introcs.assert_equals('abc', result)
+
+
+def test_get_src():
+    """
+    Test procedure for get_src
+    """
+    print('Testing get_src')
+
+
+def test_get_dst():
+    """
+    Test procedure for get_dst
+    """
+    print('Testing get_dst')
+
+
+def test_has_error():
+    """
+    Test procedure for has_error
+    """
+    print('Testing has_error')
+
+
+def test_service_response():
+    """
+    Test procedure for service_response
+    """
+    print('Testing service_response')
+
+
+def test_iscurrency():
+    """
+    Test procedure for iscurrency
+    """
+    print('Testing iscurrency')
+
+
+def test_exchange():
+    """
+    Test procedure for exchange
+    """
+    print('Testing exchange')
+
+
+test_before_space()
+test_after_space()
+test_first_inside_quotes()
+test_get_src()
+test_get_dst()
+test_has_error()
+test_service_response()
+test_iscurrency()
+test_exchange()
+
+
+print("All tests completed succesfully.")
